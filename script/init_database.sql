893d05d17188ec0cc650d7ea49a53affca0bc060
Sumedha Dewan <sumedhadewan@hotmail.com>
2025-02-08 21:36:55 +0000

    Update init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,1 +1,13 @@
+/*
+-------------------------------------------
+Purpose: Create database and create 3 schemas.
+___________________________________________
+*/
 
+-- create sql_database
+CREATE DATABASE sql_database;
+
+-- Create schemas for each layer - bronze, silver, gold
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
