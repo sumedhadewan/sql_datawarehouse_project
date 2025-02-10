8bbafef78df368db0099c2b3a5c3c9f20793da3a
Sumedha Dewan <sumedhadewan@hotmail.com>
2025-02-10 13:48:45 +0000

    Update ddl_silver.sql

--- script/silver/ddl_silver.sql
+++ script/silver/ddl_silver.sql
@@ -17,7 +17,7 @@ CREATE TABLE silver.crm_cust_info(
 	cst_marital_status VARCHAR(50),
 	cst_gndr VARCHAR(50),
 	cst_create_date DATE,
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date DATE DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.crm_prd_info;
@@ -29,8 +29,8 @@ CREATE TABLE silver.crm_prd_info(
 	prd_cost INT,
 	prd_line VARCHAR(50),
 	prd_start_dt DATE,
-	prd_end_dt DATE
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP	
+	prd_end_dt DATE,
+	dwh_create_date DATE DEFAULT CURRENT_TIMESTAMP	
 );
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
@@ -44,7 +44,7 @@ CREATE TABLE silver.crm_sales_details(
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date DATE DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_cust_az12;
@@ -52,14 +52,14 @@ CREATE TABLE silver.erp_cust_az12(
 	cid VARCHAR(50),
 	bdate DATE,
 	gen VARCHAR(50),
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date DATE DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101(
 	cid VARCHAR(50),
 	cntry VARCHAR(50),
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date DATE DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
@@ -68,5 +68,5 @@ CREATE TABLE silver.erp_px_cat_g1v2(
 	cat VARCHAR(50),
 	subcat VARCHAR(50),
 	maintenance VARCHAR(50),
-	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+	dwh_create_date DATE DEFAULT CURRENT_TIMESTAMP
 );
