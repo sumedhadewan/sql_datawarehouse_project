0e5d437006bc3cb75cc08b2f87c826738c6fee99
Sumedha Dewan <sumedhadewan@hotmail.com>
2025-02-13 23:48:47 +0000

    Update ddl_gold.sql

--- script/gold/ddl_gold.sql
+++ script/gold/ddl_gold.sql
@@ -8,7 +8,7 @@ SELECT
 	ci.cst_lastname AS last_name,
 	ci.cst_marital_status AS marital_status,
 	CASE WHEN ci.cst_gndr != 'n/a' then ci.cst_gndr
-ELSE COALESCE(ca.gen,'n/a')end AS gender, -- CRM is the master for gender information
+	ELSE COALESCE(ca.gen,'n/a')end AS gender, -- CRM is the master for gender information
 	ci.cst_create_date AS create_date,
 	ca.bdate AS brithdate,
 	la.cntry AS country
@@ -18,3 +18,36 @@ ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 AS la
 ON ci.cst_key = la.cid
 
+CREATE VIEW gold.dim_products AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY pi.prd_start_dt, pi.prd_key) AS product_key,
+    pi.prd_id AS product_id,
+    pi.prd_key AS product_number,
+    pi.prd_nm AS product_name,
+    pi.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS subcategory,
+    pc.maintenance,
+    pi.prd_cost AS cost,
+    pi.prd_line AS product_line,
+    pi.prd_start_dt AS start_date
+FROM silver.crm_prd_info pi
+LEFT JOIN silver.erp_px_cat_g1v2 pc 
+ON pi.cat_id = pc.id;
+
+CREATE VIEW gold.fact_sales AS
+SELECT 
+    sd.sls_ord_num AS order_number,
+    pr.product_key,
+    cu.customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt AS shipping_date,
+    sd.sls_due_dt AS due_date,
+    sd.sls_sales AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr 
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu 
+ON sd.sls_cust_id = cu.customer_id;
