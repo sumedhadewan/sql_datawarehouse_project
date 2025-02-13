495f1ae623841ab5cc7ab131b6fcc9984b8732dd
Sumedha Dewan <sumedhadewan@hotmail.com>
2025-02-13 18:36:20 +0000

    Update ddl_gold.sql

--- script/gold/ddl_gold.sql
+++ script/gold/ddl_gold.sql
@@ -1,1 +1,20 @@
 
+CREATE VIEW gold.dim_customers AS
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' then ci.cst_gndr
+ELSE COALESCE(ca.gen,'n/a')end AS gender, -- CRM is the master for gender information
+	ci.cst_create_date AS create_date,
+	ca.bdate AS brithdate,
+	la.cntry AS country
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS la
+ON ci.cst_key = la.cid
+
